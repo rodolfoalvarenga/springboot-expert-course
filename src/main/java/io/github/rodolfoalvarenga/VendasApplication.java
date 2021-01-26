bbb5c28a54f3bdb06824d356236ca2ba39bfd0cc
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-01-26 14:48:47 -0300

    Query Methods initial operations

--- src/main/java/io/github/rodolfoalvarenga/VendasApplication.java
+++ src/main/java/io/github/rodolfoalvarenga/VendasApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class VendasApplication {
 
@@ -20,32 +18,8 @@ public class VendasApplication {
             clientes.save(new Cliente("Rodolfo"));
             clientes.save(new Cliente("Thaís"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado");
-                clientes.save(c);
-            });
-
-            todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.findByNomeLike("ha").forEach(System.out::println);
-
-            System.out.println("Deletando clientes");
-            clientes.findAll().forEach(c -> {
-                clientes.delete(c);
-            });
-
-            todosClientes = clientes.findAll();
-            if (todosClientes.isEmpty()) {
-                System.out.println("Nenhum cliente encontrado.");
-            } else {
-                todosClientes.forEach(System.out::println);
-            }
+            boolean existe = clientes.existsByNome("Rodolfo");
+            System.out.println("Existe um cliente com o nome Rodolfo? " + existe);
         };
     }
 
