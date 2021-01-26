8cc5ea84df205998b45bcef1fc13895dfe5bed38
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-01-26 14:32:25 -0300

    JpaRepository refactoring

--- src/main/java/io/github/rodolfoalvarenga/VendasApplication.java
+++ src/main/java/io/github/rodolfoalvarenga/VendasApplication.java
@@ -17,30 +17,30 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Rodolfo"));
-            clientes.salvar(new Cliente("Thaís"));
+            clientes.save(new Cliente("Rodolfo"));
+            clientes.save(new Cliente("Thaís"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
+            List<Cliente> todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Atualizando clientes");
             todosClientes.forEach(c -> {
                 c.setNome(c.getNome() + " atualizado");
-                clientes.atualizar(c);
+                clientes.save(c);
             });
 
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Buscando clientes");
-            clientes.buscarPorNome("ha").forEach(System.out::println);
+            clientes.findByNomeLike("ha").forEach(System.out::println);
 
             System.out.println("Deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
+            clientes.findAll().forEach(c -> {
+                clientes.delete(c);
             });
 
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             if (todosClientes.isEmpty()) {
                 System.out.println("Nenhum cliente encontrado.");
             } else {
