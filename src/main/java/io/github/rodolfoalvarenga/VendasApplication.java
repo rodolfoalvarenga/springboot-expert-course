032dadd28d4b72ea07a355e3f68deb0bfaaf5f1a
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-01-27 13:18:56 -0300

    GET Cliente

--- src/main/java/io/github/rodolfoalvarenga/VendasApplication.java
+++ src/main/java/io/github/rodolfoalvarenga/VendasApplication.java
@@ -1,10 +1,23 @@
 package io.github.rodolfoalvarenga;
 
+import io.github.rodolfoalvarenga.domain.entity.Cliente;
+import io.github.rodolfoalvarenga.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+        return args -> {
+            Cliente c = new Cliente(null, "Rodolfo");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
